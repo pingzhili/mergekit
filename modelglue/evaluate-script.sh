4f47076fe522a9fd1911d9c156fb1db07bc6a15a
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-08 00:31:15 +0800

    update

---
+++ modelglue/evaluate-script.sh
@@ -0,0 +1,12 @@
+models=("checkpoints/beyonder_1" "checkpoints/beyonder_2" "checkpoints/beyonder_3" "checkpoints/beyonder_4" "checkpoints/beyonder_5")
+i=0
+for model in "${models[@]}"; do
+    ((i++))
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval \
+        --model hf \
+        --model_args pretrained=$model \
+        --tasks mmlu \
+        --num_fewshot 5 \
+        --batch_size 1 \
+        --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
+done
