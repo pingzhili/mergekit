af3e6b91ff86c905d7410e52b9096142d8d1ec93
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-15 00:12:33 +0800

    update

--- modelglue/exp-0-1-hybrid-mlp-expert/evaluate.sh
+++ modelglue/exp-0-1-hybrid-mlp-expert/evaluate.sh
@@ -6,36 +6,36 @@ for model in "${models[@]}"; do
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks gsm8k \
       --num_fewshot 5 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./5shot_gsm8k_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks arc_challenge \
       --num_fewshot 25 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks truthfulqa \
       --num_fewshot 0 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./0shot_truthfulqa_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks winogrande \
       --num_fewshot 5 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks hellaswag \
       --num_fewshot 10 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./10shot_hellaswag_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True \
       --tasks mmlu \
       --num_fewshot 5 \
-      --batch_size auto:4 \
+      --batch_size 1 \
       --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
 done
