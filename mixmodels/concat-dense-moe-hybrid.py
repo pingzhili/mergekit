31510f5a57a6b79b1470ac7ee9f9ced2cb627d3e
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-14 23:23:33 +0800

    update

--- mixmodels/concat-dense-moe-hybrid.py
+++ mixmodels/concat-dense-moe-hybrid.py
@@ -49,8 +49,8 @@ def build(
     dense_model = ModelReference.parse(dense_dir)
 
     moe_config = moe_model.config(trust_remote_code=False)
-    moe_config.auto_map['AutoModelForCausalLM'] = "modeling_mixtral_hybrid.MixtralHybridForCausalLM"
     out_config = MixtralHybridConfig(**moe_config.to_dict())
+    out_config.auto_map['AutoModelForCausalLM'] = "modeling_mixtral_hybrid.MixtralHybridForCausalLM"
     out_config.num_dense_layers = num_dense_layers
     out_config.save_pretrained(out_path)
     modeling_file_path = os.path.join(get_script_path(), "mixtral_hybrid", "modeling_mixtral_hybrid.py")
