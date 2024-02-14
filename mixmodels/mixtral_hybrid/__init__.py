206878d523d24487e13d643e09c2421db5a22df0
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-14 16:34:42 +0800

    register new model

--- mixmodels/mixtral_hybrid/__init__.py
+++ mixmodels/mixtral_hybrid/__init__.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Author: pingzhili
 # @Time: 2024/2/14
+from transformers import AutoModel, AutoModelForCausalLM, AutoConfig
+
+from .configuration_mixtral_hybrid import (
+    MixtralHybridConfig
+)
+from .modeling_mixtral_hybrid import (
+    MixtralHybridModel,
+    MixtralHybridConfig,
+    MixtralHybridForCausalLM,
+    MixtralHybridDecoderLayer
+)
+
+AutoConfig.register("mixtral_hybrid", MixtralHybridConfig)
+AutoModel.register(MixtralHybridConfig, MixtralHybridModel)
+AutoModelForCausalLM.register(MixtralHybridConfig, MixtralHybridForCausalLM)
