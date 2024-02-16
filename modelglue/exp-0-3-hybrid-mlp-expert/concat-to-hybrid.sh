24c1e7a32d8672cb5c97a1246e67a7ad07f72378
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-16 22:23:45 +0800

    update

--- modelglue/exp-0-3-hybrid-mlp-expert/concat-to-hybrid.sh
+++ modelglue/exp-0-3-hybrid-mlp-expert/concat-to-hybrid.sh
@@ -3,6 +3,6 @@ export PYTHONPATH="./mixmodels:$PYTHONPATH"
 python mixmodels/concat-dense-moe-hybrid.py \
   --dense_dir="checkpoints/exp-0-3-hybrid-mlp-expert/base-model-from-dare_ties" \
   --moe_dir="checkpoints/exp-0-3-hybrid-mlp-expert/mix-moe" \
-  --num_dense_layers=16 \
-  --out_path="checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-16" \
+  --num_dense_layers=8 \
+  --out_path="checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-8" \
   --out_dtype="bfloat16"
