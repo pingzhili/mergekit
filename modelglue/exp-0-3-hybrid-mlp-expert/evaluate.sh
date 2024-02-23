dc935b3e347b92b9e9e09472d740ad51403c6327
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-02-23 15:40:03 +0800

    update

--- modelglue/exp-0-3-hybrid-mlp-expert/evaluate.sh
+++ modelglue/exp-0-3-hybrid-mlp-expert/evaluate.sh
@@ -1,4 +1,4 @@
-models=("checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-8" "checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-16" "checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-24")
+models=("checkpoints/exp-0-3-hybrid-mlp-expert/mix-moe" "checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-8" "checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-16" "checkpoints/exp-0-3-hybrid-mlp-expert/hybrid-moe-dense-24")
 i=0
 for model in "${models[@]}"; do
     ((i++))
@@ -38,4 +38,36 @@ for model in "${models[@]}"; do
       --num_fewshot 5 \
       --batch_size 1 \
       --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+      --model $model \
+      --tasks humaneval \
+      --max_length_generation 512 \
+      --do_sample True \
+      --n_samples 1 \
+      --top_p 0.95 \
+      --batch_size 1 \
+      --temperature 0.2 \
+      --trust_remote_code \
+      --precision bf16 \
+      --allow_code_execution \
+      --use_auth_token \
+      --save_generations \
+      --save_generations_path humaneval_generations_model_idx$i.json \
+      --metric_output_path humaneval_metric_output_model_idx$i.json
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+      --model $model \
+      --tasks mbpp \
+      --max_length_generation 512 \
+      --do_sample True \
+      --n_samples 1 \
+      --top_p 0.95 \
+      --batch_size 1 \
+      --temperature 0.2 \
+      --trust_remote_code \
+      --precision bf16 \
+      --allow_code_execution \
+      --use_auth_token \
+      --save_generations \
+      --save_generations_path mbpp_generations_model_idx$i.json \
+      --metric_output_path mbpp_metric_output_model_idx$i.json
 done
