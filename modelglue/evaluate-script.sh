c40fc629f7f5dca472c04ba380fd95eca0cb20f4
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-20 21:30:08 +0800

    update

--- modelglue/evaluate-script.sh
+++ modelglue/evaluate-script.sh
@@ -1,12 +1,42 @@
-models=("mlabonne/Beyonder-4x7B-v2" "mlabonne/Marcoro14-7B-slerp" "openchat/openchat-3.5-1210" "beowolx/CodeNinja-1.0-OpenChat-7B" "maywell/PiVoT-0.1-Starling-LM-RP" "WizardLM/WizardMath-7B-V1.1")
+models=("mlabonne/Beyonder-4x7B-v2" "./checkpoints/beyonder_dare_linear" "./checkpoints/beyonder_dare_ties" "./checkpoints/beyonder_linear")
 i=0
 for model in "${models[@]}"; do
     ((i++))
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval \
-        --model hf \
-        --model_args pretrained=$model,dtype="bfloat16" \
-        --tasks mmlu \
-        --num_fewshot 5 \
-        --batch_size 1 \
-        --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks gsm8k \
+      --num_fewshot 5 \
+      --batch_size 1 \
+      --output_path ./5shot_gsm8k_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks arc_challenge \
+      --num_fewshot 25 \
+      --batch_size 1 \
+      --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks truthfulqa \
+      --num_fewshot 0 \
+      --batch_size 1 \
+      --output_path ./0shot_truthfulqa_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks winogrande \
+      --num_fewshot 5 \
+      --batch_size 1 \
+      --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks hellaswag \
+      --num_fewshot 10 \
+      --batch_size 1 \
+      --output_path ./10shot_hellaswag_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True \
+      --tasks mmlu \
+      --num_fewshot 5 \
+      --batch_size 1 \
+      --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
+
 done
