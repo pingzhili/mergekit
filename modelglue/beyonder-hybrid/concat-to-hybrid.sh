24fe38e38d84080504456a6ceada8ad335e9e986
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-21 17:33:41 +0800

    update

--- modelglue/beyonder-hybrid/concat-to-hybrid.sh
+++ modelglue/beyonder-hybrid/concat-to-hybrid.sh
@@ -1,22 +1,22 @@
 export PYTHONPATH="./mixmodels:$PYTHONPATH"
-
+root_dir="modelglue/beyonder-hybrid"
 python mixmodels/concat-dense-moe-hybrid.py \
-  --dense_dir="checkpoints/beyonder-hybrid/base-model-from-dare_ties" \
-  --moe_dir="checkpoints/beyonder-hybrid/mix-moe" \
+  --dense_dir="$root_dir/merged_model" \
+  --moe_dir="$root_dir/mix-moe" \
   --num_dense_layers=8 \
-  --out_path="checkpoints/beyonder-hybrid/hybrid-moe-dense-8" \
+  --out_path="$root_dir/hybrid-moe-dense-8" \
   --out_dtype="bfloat16"
 
 python mixmodels/concat-dense-moe-hybrid.py \
-  --dense_dir="checkpoints/beyonder-hybrid/base-model-from-dare_ties" \
-  --moe_dir="checkpoints/beyonder-hybrid/mix-moe" \
+  --dense_dir="$root_dir/merged_model" \
+  --moe_dir="$root_dir/mix-moe" \
   --num_dense_layers=16 \
-  --out_path="checkpoints/beyonder-hybrid/hybrid-moe-dense-16" \
+  --out_path="$root_dir/hybrid-moe-dense-16" \
   --out_dtype="bfloat16"
 
 python mixmodels/concat-dense-moe-hybrid.py \
-  --dense_dir="checkpoints/beyonder-hybrid/base-model-from-dare_ties" \
-  --moe_dir="checkpoints/beyonder-hybrid/mix-moe" \
+  --dense_dir="$root_dir/merged_model" \
+  --moe_dir="$root_dir/mix-moe" \
   --num_dense_layers=24 \
-  --out_path="checkpoints/beyonder-hybrid/hybrid-moe-dense-24" \
+  --out_path="$root_dir/hybrid-moe-dense-24" \
   --out_dtype="bfloat16"
