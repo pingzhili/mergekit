95b4bfd5efc432742bec24a9f89d477445e04e91
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-03-26 17:58:20 +0800

    update

--- modelglue/beyonder-block-hybrid/evaluate-dense-0.sh
+++ modelglue/beyonder-block-hybrid/evaluate-dense-0.sh
@@ -1,4 +1,3 @@
-#models=("modelglue/hybrid-moe-dense-0" "modelglue/beyonder-block-hybrid/hybrid-moe-dense-8" "modelglue/beyonder-block-hybrid/hybrid-moe-dense-16" "modelglue/beyonder-block-hybrid/hybrid-moe-dense-24")
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
     --model_args pretrained="modelglue/beyonder-block-hybrid/hybrid-moe-dense-0",trust_remote_code=True,use_cache=False \
     --tasks gsm8k \
