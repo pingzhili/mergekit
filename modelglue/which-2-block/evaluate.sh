48c7fcc54a1020b69f06bbc63b0aa2888947b925
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-04-03 18:59:42 +0800

    update

--- modelglue/which-2-block/evaluate.sh
+++ modelglue/which-2-block/evaluate.sh
@@ -4,12 +4,6 @@ for model in "${models[@]}"; do
     ((i++))
     CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks gsm8k \
-      --num_fewshot 5 \
-      --batch_size 1 \
-      --output_path ./5shot_gsm8k_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks arc_challenge \
       --num_fewshot 25 \
       --batch_size 1 \
@@ -70,4 +64,10 @@ for model in "${models[@]}"; do
       --save_generations \
       --save_generations_path mbpp_generations_model_idx$i.json \
       --metric_output_path mbpp_metric_output_model_idx$i.json
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
+      --tasks gsm8k \
+      --num_fewshot 5 \
+      --batch_size 1 \
+      --output_path ./5shot_gsm8k_model_idx_$i.json >> output_model_idx_$i.out
 done
