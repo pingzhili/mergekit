4c10256e456d99d394aa0f24d5af277161d1d59f
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-04-04 10:14:58 +0800

    update

--- modelglue/which-2-model/concat-to-model_hybrid.sh
+++ modelglue/which-2-model/concat-to-model_hybrid.sh
@@ -8,7 +8,7 @@ root_dir="modelglue/which-2-model"
 #  --num_dense_layers=16 \
 #  --out_path="$root_dir/hybrid-moe-dense-16" \
 #  --out_dtype="bfloat16"
-python mixmodels/concat-dense-moe-block-level.py \
+python mixmodels/concat-dense-moe-model-level-hybrid.py \
   --expert1_dir="meta-llama/Llama-2-7b-chat-hf" \
   --expert2_dir="lmsys/vicuna-7b-v1.5" \
   --dense_dir="$root_dir/merged_model" \
