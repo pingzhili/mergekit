6a5d9021f10f48de3a91ca3a38b53a91d0ffdb0e
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-03 16:28:07 +0800

    update

--- modelglue/which-4/evaluate-full-mixture.sh
+++ modelglue/which-4/evaluate-full-mixture.sh
@@ -2,25 +2,25 @@ models=("modelglue/which-4/ffn_mixed_moe")
 i=4
 for model in "${models[@]}"; do
     ((i++))
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks arc_challenge \
       --num_fewshot 25 \
       --batch_size 1 \
       --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks winogrande \
       --num_fewshot 5 \
       --batch_size 1 \
       --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks mmlu \
       --num_fewshot 5 \
       --batch_size 1 \
       --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002 ../bigcode-evaluation-harness/main.py \
       --model $model \
       --tasks humaneval \
       --max_length_generation 512 \
@@ -36,7 +36,7 @@ for model in "${models[@]}"; do
       --save_generations \
       --save_generations_path humaneval_generations_model_idx$i.json \
       --metric_output_path humaneval_metric_output_model_idx$i.json
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002 ../bigcode-evaluation-harness/main.py \
       --model $model \
       --tasks mbpp \
       --max_length_generation 512 \
@@ -52,7 +52,7 @@ for model in "${models[@]}"; do
       --save_generations \
       --save_generations_path mbpp_generations_model_idx$i.json \
       --metric_output_path mbpp_metric_output_model_idx$i.json
-    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 21002  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks gsm8k \
       --num_fewshot 5 \
