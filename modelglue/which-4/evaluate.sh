f2884504ca6047e72ef1efce2e71f75380a8ab42
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-03 10:13:07 +0800

    update

--- modelglue/which-4/evaluate.sh
+++ modelglue/which-4/evaluate.sh
@@ -8,24 +8,24 @@ for model in "${models[@]}"; do
       --num_fewshot 25 \
       --batch_size 1 \
       --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks truthfulqa \
-      --num_fewshot 0 \
-      --batch_size 1 \
-      --output_path ./0shot_truthfulqa_model_idx_$i.json >> output_model_idx_$i.out
+#    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+#      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
+#      --tasks truthfulqa \
+#      --num_fewshot 0 \
+#      --batch_size 1 \
+#      --output_path ./0shot_truthfulqa_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks winogrande \
       --num_fewshot 5 \
       --batch_size 1 \
       --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks hellaswag \
-      --num_fewshot 10 \
-      --batch_size 1 \
-      --output_path ./10shot_hellaswag_model_idx_$i.json >> output_model_idx_$i.out
+#    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+#      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
+#      --tasks hellaswag \
+#      --num_fewshot 10 \
+#      --batch_size 1 \
+#      --output_path ./10shot_hellaswag_model_idx_$i.json >> output_model_idx_$i.out
     CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks mmlu \
