744ec3d479bbe9b0a7954e0b0bdfd5e91fbd2e67
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-03 11:15:40 +0800

    update

--- modelglue/which-4/evaluate.sh
+++ modelglue/which-4/evaluate.sh
@@ -2,37 +2,25 @@ models=("modelglue/which-4/hybrid_ffn_lv_mixed_moe" "modelglue/which-4/hybrid_bl
 i=0
 for model in "${models[@]}"; do
     ((i++))
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks arc_challenge \
-      --num_fewshot 25 \
-      --batch_size 1 \
-      --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
-#    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+#    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
 #      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-#      --tasks truthfulqa \
-#      --num_fewshot 0 \
+#      --tasks arc_challenge \
+#      --num_fewshot 25 \
 #      --batch_size 1 \
-#      --output_path ./0shot_truthfulqa_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks winogrande \
-      --num_fewshot 5 \
-      --batch_size 1 \
-      --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
-#    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+#      --output_path ./25shot_arc_challenge_model_idx_$i.json >> output_model_idx_$i.out
+#    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
 #      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-#      --tasks hellaswag \
-#      --num_fewshot 10 \
+#      --tasks winogrande \
+#      --num_fewshot 5 \
 #      --batch_size 1 \
-#      --output_path ./10shot_hellaswag_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
-      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
-      --tasks mmlu \
-      --num_fewshot 5 \
-      --batch_size 1 \
-      --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+#      --output_path ./5shot_winogrande_model_idx_$i.json >> output_model_idx_$i.out
+#    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+#      --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
+#      --tasks mmlu \
+#      --num_fewshot 5 \
+#      --batch_size 1 \
+#      --output_path ./5shot_mmlu_model_idx_$i.json >> output_model_idx_$i.out
+    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
       --model $model \
       --tasks humaneval \
       --max_length_generation 512 \
@@ -48,7 +36,7 @@ for model in "${models[@]}"; do
       --save_generations \
       --save_generations_path humaneval_generations_model_idx$i.json \
       --metric_output_path humaneval_metric_output_model_idx$i.json
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
+    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001 ../bigcode-evaluation-harness/main.py \
       --model $model \
       --tasks mbpp \
       --max_length_generation 512 \
@@ -64,7 +52,7 @@ for model in "${models[@]}"; do
       --save_generations \
       --save_generations_path mbpp_generations_model_idx$i.json \
       --metric_output_path mbpp_metric_output_model_idx$i.json
-    CUDA_VISIBLE_DEVICES=3,4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
+    CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 21001  -m lm_eval --model hf \
       --model_args pretrained=$model,trust_remote_code=True,use_cache=False \
       --tasks gsm8k \
       --num_fewshot 5 \
