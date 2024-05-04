c505cc647ab5349fa00622c45877bf5f66530983
pingzhi <pingzhili@mail.ustc.edu.cn>
2024-05-04 11:05:26 +0800

    update

---
+++ modelglue/which-4/concat-to-full.sh
@@ -0,0 +1,28 @@
+export PYTHONPATH="./mixmodels:$PYTHONPATH"
+root_dir="modelglue/which-4"
+python mixmodels/concat-dense-moe-ffn-level-hybrid.py \
+  --dense_dir="$root_dir/linear_merged_model" \
+  --moe_dir="$root_dir/ffn_mixed_moe" \
+  --num_dense_layers=0 \
+  --out_path="$root_dir/full_ffn_lv_mixed_moe" \
+  --out_dtype="bfloat16"
+#python mixmodels/concat-dense-moe-block-level-hybrid.py \
+#  --expert1_dir="migtissera/Synthia-7B-v1.2" \
+#  --expert2_dir="neuralmagic/Llama-2-7b-evolcodealpaca" \
+#  --expert3_dir="PygmalionAI/pygmalion-2-7b" \
+#  --expert4_dir="meta-math/MetaMath-7B-V1.0" \
+#  --dense_dir="$root_dir/linear_merged_model" \
+#  --moe_dir="$root_dir/ffn_mixed_moe" \
+#  --num_dense_layers=0 \
+#  --out_path="$root_dir/full_block_lv_mixed_moe" \
+#  --out_dtype="bfloat16"
+#python mixmodels/concat-dense-moe-model-level-hybrid.py \
+#  --expert1_dir="migtissera/Synthia-7B-v1.2" \
+#  --expert2_dir="neuralmagic/Llama-2-7b-evolcodealpaca" \
+#  --expert3_dir="PygmalionAI/pygmalion-2-7b" \
+#  --expert4_dir="meta-math/MetaMath-7B-V1.0" \
+#  --dense_dir="$root_dir/linear_merged_model" \
+#  --moe_dir="$root_dir/ffn_mixed_moe" \
+#  --num_dense_layers=0 \
+#  --out_path="$root_dir/full_model_lv_mixed_moe" \
+#  --out_dtype="bfloat16"
--- modelglue/which-4/concat-to-hybrid.sh
+++ modelglue/which-4/concat-to-hybrid.sh
@@ -1,31 +1,11 @@
 export PYTHONPATH="./mixmodels:$PYTHONPATH"
 root_dir="modelglue/which-4"
-#python mixmodels/concat-dense-moe-ffn-level-hybrid.py \
-#  --dense_dir="$root_dir/linear_merged_model" \
-#  --moe_dir="$root_dir/ffn_mixed_moe" \
-#  --num_dense_layers=16 \
-#  --out_path="$root_dir/hybrid_ffn_lv_mixed_moe" \
-#  --out_dtype="bfloat16"
-#python mixmodels/concat-dense-moe-block-level-hybrid.py \
-#  --expert1_dir="migtissera/Synthia-7B-v1.2" \
-#  --expert2_dir="neuralmagic/Llama-2-7b-evolcodealpaca" \
-#  --expert3_dir="PygmalionAI/pygmalion-2-7b" \
-#  --expert4_dir="meta-math/MetaMath-7B-V1.0" \
-#  --dense_dir="$root_dir/linear_merged_model" \
-#  --moe_dir="$root_dir/ffn_mixed_moe" \
-#  --num_dense_layers=16 \
-#  --out_path="$root_dir/hybrid_block_lv_mixed_moe" \
-#  --out_dtype="bfloat16"
-#python mixmodels/concat-dense-moe-model-level-hybrid.py \
-#  --expert1_dir="migtissera/Synthia-7B-v1.2" \
-#  --expert2_dir="neuralmagic/Llama-2-7b-evolcodealpaca" \
-#  --expert3_dir="PygmalionAI/pygmalion-2-7b" \
-#  --expert4_dir="meta-math/MetaMath-7B-V1.0" \
-#  --dense_dir="$root_dir/linear_merged_model" \
-#  --moe_dir="$root_dir/ffn_mixed_moe" \
-#  --num_dense_layers=16 \
-#  --out_path="$root_dir/hybrid_model_lv_mixed_moe" \
-#  --out_dtype="bfloat16"
+python mixmodels/concat-dense-moe-ffn-level-hybrid.py \
+  --dense_dir="$root_dir/linear_merged_model" \
+  --moe_dir="$root_dir/ffn_mixed_moe" \
+  --num_dense_layers=16 \
+  --out_path="$root_dir/hybrid_ffn_lv_mixed_moe" \
+  --out_dtype="bfloat16"
 python mixmodels/concat-dense-moe-block-level-hybrid.py \
   --expert1_dir="migtissera/Synthia-7B-v1.2" \
   --expert2_dir="neuralmagic/Llama-2-7b-evolcodealpaca" \
@@ -33,8 +13,8 @@ python mixmodels/concat-dense-moe-block-level-hybrid.py \
   --expert4_dir="meta-math/MetaMath-7B-V1.0" \
   --dense_dir="$root_dir/linear_merged_model" \
   --moe_dir="$root_dir/ffn_mixed_moe" \
-  --num_dense_layers=0 \
-  --out_path="$root_dir/full_block_lv_mixed_moe" \
+  --num_dense_layers=16 \
+  --out_path="$root_dir/hybrid_block_lv_mixed_moe" \
   --out_dtype="bfloat16"
 python mixmodels/concat-dense-moe-model-level-hybrid.py \
   --expert1_dir="migtissera/Synthia-7B-v1.2" \
@@ -43,6 +23,6 @@ python mixmodels/concat-dense-moe-model-level-hybrid.py \
   --expert4_dir="meta-math/MetaMath-7B-V1.0" \
   --dense_dir="$root_dir/linear_merged_model" \
   --moe_dir="$root_dir/ffn_mixed_moe" \
-  --num_dense_layers=0 \
-  --out_path="$root_dir/full_model_lv_mixed_moe" \
+  --num_dense_layers=16 \
+  --out_path="$root_dir/hybrid_model_lv_mixed_moe" \
   --out_dtype="bfloat16"
